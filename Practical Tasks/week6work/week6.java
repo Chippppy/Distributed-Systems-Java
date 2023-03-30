3323560adc08a110d242ff9c5a2387a1782084bf
Chippppy <hazachipman@gmail.com>
2023-03-30 06:52:38 +0000

    Week 6 Work completed! Annoying one line change fixed a lot! Also shown that I'll need to refactor main client. Will do soon.

--- Practical Tasks/week6work/week6.java
+++ Practical Tasks/week6work/week6.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Arrays;
 
-public class client {
+public class week6 {
     
     public static void main(String[] args) {  
         try {    
@@ -34,27 +34,54 @@ public class client {
 			// GET ALL SERVER INFORMATION & SAVE LARGEST TYPE WITH #OF LARGEST SERVERS APPENDED INTO RESPONSE ARRAY
 			String[] largestServer = getLargestServer(s, dout, dis);
 
-			// START LOOP BETWEEN SERVER AND CLIENT WITH RECURSION & FIRST REDY RESPONSE
-			commandHandler(s, dout, dis, response, largestServer);
+			loop: while(true) {
+                String commandType = response.split(" ")[0];
+				//System.out.println(commandType);
+                switch(commandType) {
+                    case ("JOBN"):
+                        response = scheduleJob(s, dout, dis, response, largestServer);
+                        break;
+
+                    case ("JOBP"):
+
+                    case ("JCPL"):
+                        sendBasicCommand(s, dout, dis, "REDY");
+                        response = readResponse(s, dout, dis);
+                        break;
+
+                    case ("RESF"):
+
+                    case ("RESR"):
+
+                    case ("CHKQ"):
+
+					case ("NONE"):
+						quitSession(s, dout, dis);
+						break loop;
+
+                    default:
+                        //System.out.println("Switch Case Failed to Default");
+                }
+            }
 
 			// COMMAND HELPER WILL ALWAYS FINISH WITH THE 'quitSession' FUNCTION & THEN CLOSE RESOURCES BELOW.
             dout.close();
             s.close();
         } catch(Exception e) {
-            System.out.println(e);
+            System.err.println(e);
         }
     }
 
 	public static String readResponse(Socket s, DataOutputStream output, BufferedReader input) throws IOException {
 		// READ RESPONSE
 		String str=(String)input.readLine();  
-        System.out.println("C: Recv " + str + ", FROM SERVER\n");
+        //System.out.println("C: Recv " + str + ", FROM SERVER\n");
 		return str;
 	}
 
 	public static void sendBasicCommand (Socket s, DataOutputStream output, BufferedReader input, String command) throws IOException {
 		// SEND COMMAND
-		System.out.println("C: Send "+command+"\n");
+		//System.out.println("C: Send "+command+"\n");
 		output.write((command+"\n").getBytes());
 		output.flush();
 	}
@@ -85,7 +112,7 @@ public class client {
 	}
 
 	public static String[] getLargestServer(Socket s, DataOutputStream output, BufferedReader input) throws IOException {
-		System.out.println("Getting largest Server");
+		// System.out.println("Getting largest Server");
 		// SEND GETS
 		sendBasicCommand(s, output, input, "GETS All");
 
@@ -93,9 +120,9 @@ public class client {
 		String str = (String)input.readLine();
 		String[] data = str.split(" ");
 		int nRecs = Integer.parseInt(data[1]);
-		int recSize = Integer.parseInt(data[2]);
-		System.out.println("nRecs = "+nRecs+"\nrecSize = "+recSize+"\n");
-		System.out.println("C: Recv " + str + ", FROM SERVER\n");
+		//int recSize = Integer.parseInt(data[2]);
+		//System.out.println("nRecs = "+nRecs+"\nrecSize = "+recSize+"\n");
+		//System.out.println("C: Recv " + str + ", FROM SERVER\n");
 
 		// SEND OK
 		sendBasicCommand(s, output, input, "OK");
@@ -108,7 +135,7 @@ public class client {
 		for(int i=0;i<nRecs;i++) {
 			//READ ALL SERVER RESPONSE LINES
 			str = (String)input.readLine();
-			System.out.println("C: Recv " + str + ", FROM SERVER");
+			//System.out.println("C: Recv " + str + ", FROM SERVER");
 			String[] serverData = str.split(" ");
 			int serverCores = Integer.parseInt(serverData[4]);
 			if(serverCores > maxNumCores) {
@@ -121,54 +148,21 @@ public class client {
 			}
 		}
 
-		System.out.println("Max # Cores = "+maxNumCores);
-		System.out.println("# Servers with those cores = "+numServersWithMax);
-		System.out.println("Largest Server Type = "+largestServerType);
+		// System.out.println("Max # Cores = "+maxNumCores);
+		// System.out.println("# Servers with those cores = "+numServersWithMax);
+		// System.out.println("Largest Server Type = "+largestServerType);
 
 		largestServer = Arrays.copyOf(largestServer, largestServer.length+1);
 		largestServer[largestServer.length-1] = Integer.toString(numServersWithMax);
-		System.out.println(Arrays.toString(largestServer));
+		// System.out.println(Arrays.toString(largestServer));
 
 		// SEND OK
 		sendBasicCommand(s, output, input, "OK");
 
-		// READ RESPONSE
 		readResponse(s, output, input);
 
 		// RETURN LARGEST SERVER DATA WITH # OF LARGEST SERVERS APPENDED TO END OF ARRAY
 		return largestServer;
-	}
-
-	public static void commandHandler(Socket s, DataOutputStream output, BufferedReader input, String command, String[] largestServer) throws IOException {
-		String commandType = command.split(" ")[0];
-		String response = "";
-		switch(commandType) {
-			case ("JOBN"):
-				response = scheduleJob(s, output, input, command, largestServer);
-				commandHandler(s, output, input, response, largestServer);
-				break;
-
-			case ("JOBP"):
-
-			case ("JCPL"):
-				sendBasicCommand(s, output, input, "REDY");
-				response = readResponse(s, output, input);
-				commandHandler(s, output, input, response, largestServer);
-				break;
-
-			case ("RESF"):
-
-			case ("RESR"):
-
-			case ("CHKQ"):
-
-			case ("NONE"):
-				quitSession(s, output, input);
-				break;
-
-			default:
-				System.out.println("Switch Case Failed to Default");
-		}
 	}
 	
 	public static String scheduleJob(Socket s, DataOutputStream output, BufferedReader input, String job, String[] largestServer) throws IOException {
@@ -182,7 +176,7 @@ public class client {
 		String schdString = "SCHD "+jobIndex+" "+serverType+" "+serverIndex;
 		
 		// SCHEDULE THE JOB USING SCHD
-		System.out.println("C: Send '"+schdString+"' to server\n");
+		//System.out.println("C: Send '"+schdString+"' to server\n");
 		output.write((schdString+"\n").getBytes());
 		output.flush();
 
