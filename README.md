3e2594bebc31946d4f4e686be34910efd0ff5978
Chippppy <hazachipman@gmail.com>
2023-04-01 07:13:18 +0000

    Stage1 Marking Commit. Cleaned repository of practical work files. Updated README. Removed unecessary lines of code from client that didn't change functionality.

--- README.md
+++ README.md
@@ -1,5 +1,54 @@
 # COMP6105 Project
+
 Author - Harry Chipman
 StudentID - 46358749
 Comp6105/3100 project git repository.
 
+---
+
+## Description
+
+This repository is the host of the code for the Macquarie University Assignment 'Stage1' from unit COMP6105/3100.
+
+The Author is a student of the unit. 
+
+This project was to build a client using the programming language Java, to interact in a server-client relationship to simulate Distributed Systems.
+
+The server (ds-sim) was written in the C language, and therefore the aim was to have two systems talk to each other,  completing tasks, with seemingly no compatability. 
+
+The distributed system simulation is a job scheduling simulation where after an initial authentication handshake, the server sends jobs it has to the client. It is then the clients job to work as a 'Backend' to appropriately return what jobs should be allocated to which 'servers'. 
+
+
+## Client Ability/Status
+
+Currently, the client side is defaulted (and only able) to schedule the jobs with the server using a 'LLR' algorithm (Largest Round Robin). This algorithm schedules all jobs to the first server type with the largest number of cores.
+
+There is currently no flags/options to be used when running the client code.
+
+All print statements have been removed from the client side code (as a requirement), besides the singular 'System.err.println()' statement within the main method catch statement. 
+
+
+## Usage
+
+The currently pre-compiled 'client.class' file has been pre-compiled on an M1 Mac running an Ubuntu Virtual Machine. As such this file may not run correctly on your machine.
+
+It is suggested that all users recompile the client.java file using the below steps before running the client.class file.
+
+1. Ensure that all necessary Java requirements are installed on your machine for you to compile and run Java code.
+2. Open a terminal session and navigate to the directory holding the 'client.java' file (this directory).
+3. Compile the 'client.java' file using the command;
+
+   ```bash
+   C:Your/Current/Directory javac client.java
+   ```
+4. Intialise the server runtime appropriately, E.g. run the below;
+
+   ```bash
+   C:Directory/That/Holds/ds-sim/server ./ds-server -n -c ../../configs/sample-configs/sample-config01.xml
+   ```
+5. In a new, seperate, terminal, run the newly compiled 'client.class' file. E.g. run below;
+
+   ```bash
+   C:Your/Current/Directory java client
+   ```
+6. Return to your server terminal session to view the results of the client and server interation.
--- client.java
+++ client.java
@@ -18,28 +18,20 @@ public class client {
             
 			//INITIATE HANDSHAKE BETWEEN SERVER AND CLIENT
 			String firstResponse = initHandshake(s, dout, dis);
-
-			// CHECK IF HANDSHAKE WAS SUCCESSFUL. IF NOT SUCCESSFUL - QUIT SESSION.
-			// (Always successful, same config every runtime, would leave in real life with more unstable code, 
-			// for this assignment, I'd probably delete it if I was only submitting the code itself, no report)
 			if(firstResponse.compareTo("OK") != 0) {
 				quitSession(s, dout, dis);
 			};
 			
 			// SEND INITIAL/FIRST REDY COMMAND
 			sendBasicCommand(s, dout, dis, "REDY");
-
-			// READ FIRST RESPONSE
 			String response = readResponse(s, dout, dis);
 
 			// GET ALL SERVER INFORMATION & SAVE LARGEST TYPE WITH #OF LARGEST SERVERS APPENDED INTO RESPONSE ARRAY
 			String[] largestServer = getLargestServer(s, dout, dis);
 
-			// START WHILE LOOP. USING WHILE LOOP LABELING TO BREAK OUT ON SERVER RESPONSE OF NONE.
+			// START WHILE LOOP. USING JAVA WHILE LOOP LABELING TO BREAK OUT ON SERVER RESPONSE OF NONE.
 			outerLoop: while(true) {
-				// GET THE COMMAND TYPE FROM THE SERVER RESPONSE AND SELECT FUNCTION THAT APPROPRIATELY DEALS WITH THE COMMAND.
                 String commandType = response.split(" ")[0];
-				//System.out.println(commandType);
                 switch(commandType) {
                     case ("JOBN"):
                         response = scheduleJob(s, dout, dis, response, largestServer);
@@ -60,17 +52,17 @@ public class client {
 
 					case ("NONE"):
 						quitSession(s, dout, dis);
-						break outerLoop; // This enables the breaking of the While loop.
+						break outerLoop; 
 
                     default:
-                        //System.out.println("Switch Case Failed to Default");
+
                 }
             }
 
+			// CLOSE RESOURCES WHEN FINISHED ALL ACTIONS.
             dout.close();
             s.close();
         } catch(Exception e) {
-			// NOT COMMENTING OUT ERROR STATEMENT PURPOSEFULLY.
             System.err.println(e);
         }
     }
@@ -85,7 +77,6 @@ public class client {
  */
 	public static String readResponse(Socket s, DataOutputStream output, BufferedReader input) throws IOException {
 		String str=(String)input.readLine();  
-        //System.out.println("C: Recv " + str + ", FROM SERVER\n");
 		return str;
 	}
 
@@ -98,7 +89,6 @@ public class client {
  * @param command The command to send to the server.
  */
 	public static void sendBasicCommand (Socket s, DataOutputStream output, BufferedReader input, String command) throws IOException {
-		//System.out.println("C: Send "+command+"\n");
 		output.write((command+"\n").getBytes());
 		output.flush();
 	}
@@ -113,20 +103,15 @@ public class client {
  * @return The response from the server.
  */
 	public static String initHandshake(Socket s, DataOutputStream output, BufferedReader input) throws IOException {
-		// SEND HELO
 		sendBasicCommand(s, output, input, "HELO");
 
-		// READ RESPONSE
 		String str = readResponse(s, output, input);
 
-		// SEND AUTH COMMAND
 		String username = System.getProperty("user.name");
 		sendBasicCommand(s, output, input, "AUTH "+username);
 
-		// READ RESPONSE
 		str = readResponse(s, output, input);
 
-		// RETURN RESPONSE TO CHECK IF CONNECTED
 		return str;
 	}
 
@@ -153,15 +138,11 @@ public class client {
  * array.
  */
 	public static String[] getLargestServer(Socket s, DataOutputStream output, BufferedReader input) throws IOException {
-		// System.out.println("Getting largest Server");
 		sendBasicCommand(s, output, input, "GETS All");
 
 		String str = (String)input.readLine();
 		String[] data = str.split(" ");
 		int nRecs = Integer.parseInt(data[1]);
-		//int recSize = Integer.parseInt(data[2]);
-		//System.out.println("nRecs = "+nRecs+"\nrecSize = "+recSize+"\n");
-		//System.out.println("C: Recv " + str + ", FROM SERVER\n");
 
 		sendBasicCommand(s, output, input, "OK");
 
@@ -171,9 +152,7 @@ public class client {
 		String[] largestServer = new String[11];
 
 		for(int i=0;i<nRecs;i++) {
-			//READ ALL SERVER RESPONSE LINES
 			str = (String)input.readLine();
-			//System.out.println("C: Recv " + str + ", FROM SERVER");
 			String[] serverData = str.split(" ");
 			int serverCores = Integer.parseInt(serverData[4]);
 			if(serverCores > maxNumCores) {
@@ -186,13 +165,8 @@ public class client {
 			}
 		}
 
-		// System.out.println("Max # Cores = "+maxNumCores);
-		// System.out.println("# Servers with those cores = "+numServersWithMax);
-		// System.out.println("Largest Server Type = "+largestServerType);
-
 		largestServer = Arrays.copyOf(largestServer, largestServer.length+1);
 		largestServer[largestServer.length-1] = Integer.toString(numServersWithMax);
-		// System.out.println(Arrays.toString(largestServer));
 
 		sendBasicCommand(s, output, input, "OK");
 
@@ -223,8 +197,7 @@ public class client {
 
 		String schdString = "SCHD "+jobIndex+" "+serverType+" "+serverIndex;
 		
-		// SCHEDULE THE JOB USING SCHD
-		//System.out.println("C: Send '"+schdString+"' to server\n");
+		// SCHEDULE THE JOB USING PREPARED SCHD STRING ABOVE.
 		output.write((schdString+"\n").getBytes());
 		output.flush();
 
